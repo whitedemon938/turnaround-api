d4455036e81a40f185498341b4556a627bab0be2
White Demon <133562822+whitedemon938@users.noreply.github.com>
2024-09-10 22:28:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,4 +43,5 @@ def make_response(captcha_key):
     return flask.jsonify({"status": "success", "token": captcha_key})
 
 if __name__ == "__main__":
-    app.run(debug=off)
+    app.run(debug=False)
+
